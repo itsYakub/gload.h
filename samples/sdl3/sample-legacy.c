6a534695a5c0a3c325f21efa8861e41fe82a9f9a
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-11-15 06:35:46 +0100

    samples: added SDL3 samples
    Added samples for SDL3 library, including legacy and modern OpenGL (non-DSA and DSA).

---
+++ samples/sdl3/sample-legacy.c
@@ -0,0 +1,53 @@
+#define GLOAD_IMPLEMENTATION
+#include <gload/gload.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_events.h>
+
+
+
+int main(void) {
+    SDL_Window      *window;
+    SDL_GLContext   context;
+    SDL_Event       event;
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) { return (1); }
+
+    window = SDL_CreateWindow("gload.h - SDL3 sample", 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) { return (1); }
+
+    context = SDL_GL_CreateContext(window);
+    if (!context) { return (1); }
+
+    if (!SDL_GL_MakeCurrent(window, context)) { return (1); }
+
+    if (!gloadLoadGLLoader((t_gloadLoader) SDL_GL_GetProcAddress)) { return (1); }
+
+    for (int exit = 0; !exit; ) {
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glBegin(GL_QUADS);
+            glVertex2f(-0.5,-0.5);
+            glVertex2f(-0.5, 0.5);
+            glVertex2f( 0.5, 0.5);
+            glVertex2f( 0.5,-0.5);
+        glEnd();
+
+
+        SDL_GL_SwapWindow(window);
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case (SDL_EVENT_QUIT): { exit = 1; } break;
+            }
+        }
+    }
+
+
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return (0);
+}
---
+++ samples/sdl3/sample-modern46.c
@@ -0,0 +1,134 @@
+#define GLOAD_IMPLEMENTATION
+#include <gload/gload.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_events.h>
+
+
+
+static const GLchar *g_vertex_glsl =
+"#version 330 core\n"
+"\n"
+"layout (location=0)\n"
+"in vec3    a_pos;\n"
+"\n"
+"void main() {\n"
+"   gl_Position = vec4(a_pos, 1.0);\n"
+"}\n";
+
+
+static const GLchar *g_fragment_glsl =
+"#version 330 core\n"
+"\n"
+"out vec4   f_col;\n"
+"\n"
+"void main() {\n"
+"   f_col = vec4(1.0, 1.0, 1.0, 1.0);\n"
+"}\n";
+
+
+GLfloat g_vertices[] = {
+    -0.5, -0.5, 0.0,
+    -0.5,  0.5, 0.0,
+     0.5, -0.5, 0.0,
+     0.5,  0.5, 0.0
+};
+
+
+GLuint  g_indices[] = {
+    0, 1, 2,
+    1, 2, 3
+};
+
+
+
+int main(void) {
+    SDL_Window      *window;
+    SDL_GLContext   context;
+    SDL_Event       event;
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) { return (1); }
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4)) { return (1); }
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6)) { return (1); }
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE)) { return (1); }
+
+    window = SDL_CreateWindow("gload.h - SDL3 sample", 800, 600, SDL_WINDOW_OPENGL);
+    if (!window) { return (1); }
+
+    context = SDL_GL_CreateContext(window);
+    if (!context) { return (1); }
+
+    if (!SDL_GL_MakeCurrent(window, context)) { return (1); }
+
+    if (!gloadLoadGLLoader((t_gloadLoader) SDL_GL_GetProcAddress)) { return (1); }
+    
+
+    GLuint      shader,
+                sh_v, sh_f;
+    
+    sh_v = glCreateShader(GL_VERTEX_SHADER);
+    sh_f = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(sh_v, 1, &g_vertex_glsl, 0);
+    glShaderSource(sh_f, 1, &g_fragment_glsl, 0);
+    
+    glCompileShader(sh_v);
+    glCompileShader(sh_f);
+
+    shader = glCreateProgram();
+    glAttachShader(shader, sh_v);
+    glAttachShader(shader, sh_f);
+    glLinkProgram(shader);
+
+    glDeleteShader(sh_v), sh_v = 0;
+    glDeleteShader(sh_f), sh_f = 0;
+    
+
+    GLuint      vao,
+                vbo, ibo;
+
+    glCreateVertexArrays(1, &vao);
+    glCreateBuffers(1, &vbo);
+    glCreateBuffers(1, &ibo);
+    
+    glNamedBufferData(vbo, sizeof(g_vertices), g_vertices, GL_STATIC_DRAW);
+    glNamedBufferData(ibo, sizeof(g_indices), g_indices, GL_STATIC_DRAW);
+
+    glEnableVertexArrayAttrib(vao, 0);
+    glVertexArrayAttribBinding(vao, 0, 0);
+    glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0 * sizeof(GLfloat));
+
+    glVertexArrayVertexBuffer(vao, 0, vbo, 0, 3 * sizeof(GLfloat));
+    glVertexArrayElementBuffer(vao, ibo);
+
+
+    for (int exit = 0; !exit; ) {
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
+        SDL_GL_SwapWindow(window);
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case (SDL_EVENT_QUIT): { exit = 1; } break;
+            }
+        }
+    }
+   
+
+    glDeleteBuffers(1, &vbo), vbo = 0;
+    glDeleteBuffers(1, &ibo), ibo = 0;
+    glDeleteVertexArrays(1, &vao), vao = 0;
+    glDeleteProgram(shader), shader = 0;
+
+    SDL_GL_DestroyContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return (0);
+}
