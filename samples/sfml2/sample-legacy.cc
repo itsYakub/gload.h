7880b2b4112f4196a0db5a1dddcab9e54e15670a
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-11-19 06:45:50 +0100

    samples: sfml2/ preservation
    sfml2/ sample was brought back so you can now test gload.h against both sfml2 and sfml3.

---
+++ samples/sfml2/sample-legacy.cc
@@ -0,0 +1,44 @@
+#define GLOAD_IMPLEMENTATION
+#include "./../../gload.h"
+
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Window.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+
+int main(void) {
+    sf::Window  window(sf::VideoMode( { 800, 600 } ), "gload.h - SFML sample", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event   event;
+
+    if (!window.setActive()) { return (1); }
+    if (!gloadLoadGL()) { return (1); }
+
+    while (window.isOpen()) {
+        window.display();
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case (sf::Event::Closed): {
+                    window.close();
+                } break;
+
+                default: { } break;
+            }
+        }
+        
+        
+        glClearColor(0.1, 0.1, 0.1, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glBegin(GL_QUADS);
+            glVertex2f(-0.5,-0.5);
+            glVertex2f(-0.5, 0.5);
+            glVertex2f( 0.5, 0.5);
+            glVertex2f( 0.5,-0.5);
+        glEnd();
+    }
+    
+    if (!gloadUnloadGL()) { return (1); }
+    
+    return (0);
+}
